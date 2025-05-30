d3ba834c77941cd96f4aab38be0cb741a2ddfc06
Robson Rocha <robsonrccha@gmail.com>
2025-05-30 12:10:55 -0300

    Aula 02

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,7 @@
+#CRIE UM SCRIPT PYTHON QUE LEIA O NOME DE UMA PESSOA
+# E MOSTRE UMA MENSAGEM DE BOAS-VINDAS COM O VALOR DIGITADO
+#Aula 4
+
+nome = input('Qual seu nome?: ')
+
+print(f'Bem vindo {nome}, vamos codar?!')
