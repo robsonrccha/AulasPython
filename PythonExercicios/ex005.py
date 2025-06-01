a4025117c046a6410367c45793fc7daa9f1ff50b
Robson Rocha <robsonrccha@gmail.com>
2025-06-01 20:41:17 -0300

    Revert "exercicios"
    This reverts commit 8a3de665ec6114ef454dfe2bcb930af82a02e67e.

--- PythonExercicios/ex005.py
+++
@@ -1,8 +0,0 @@
-#AULA07
-
-#FAÇA UM PROGRAMA QUE LEIA UM NUMERO INTEIRO
-#E MOSTRE NA TELA O SEU SUCESSOR E SEU ANTECESSOR
-
-num = int(input('Escolha um numero inteiro: '))
-
-print(f'Voce escolheu o numero {num}, seu antecessor é o {num -1} e o sucessor é o {num+1}.')
