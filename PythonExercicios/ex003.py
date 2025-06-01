3430d5c9a722291537f7012ada0d1be5bc601280
Robson Rocha <robsonrccha@gmail.com>
2025-06-01 20:48:55 -0300

    Aula 06

--- PythonExercicios/ex003.py
+++ PythonExercicios/ex003.py
@@ -1,3 +1,5 @@
+#AULA 06
+
 #FACA UM PROGRAMA QUE LEIA DOIS NUMEROS E MOSTRE A SOMA ENTRE ELES
 
 n1 = int(input('Digite um valor: '))
@@ -5,5 +7,3 @@ n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
 
 print(f'A soma entre {n1} e {n2} é {soma}')
-
-#02
