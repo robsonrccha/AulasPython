f96ee7402c85327e86e16c4c1a9dfcf31ca5cb67
Robson Rocha <robsonrccha@gmail.com>
2025-06-01 21:14:23 -0300

    Aula 07

--- Exercicios/ex008.py
+++ Exercicios/ex008.py
@@ -3,9 +3,9 @@
 #ESCREVA UM PROGRAMA QUE LEIA O VALOR EM METROS
 #E O EXIBA CONVERTIDO EM CENTIMETROS E MILIMETROS
 
-metros = int(input('Digite um valor em metros: '))
+metros = float(input('Digite um valor em metros: '))
 
 centimetros = metros * 100
 milimetros = metros * 1000
 
-print(f'Você escolheu {metros}m, isso dá {centimetros}cm e {milimetros}mm.')
+print(f'Você escolheu {metros}m, isso dá {centimetros:.0f}cm e {milimetros:.0f}mm.')
