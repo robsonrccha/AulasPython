a351b6bb9bfb3ac4805fbe5a9a053424a2dfdd3a
Robson Rocha <robsonrccha@gmail.com>
2025-06-01 21:19:01 -0300

    Aula 07

--- Exercicios/ex009.py
+++ Exercicios/ex009.py
@@ -1,13 +1,13 @@
 #AULA 07
 
-#FAÇA UM PROGRAMA QUE ELAI UM NUMERO INTEIRO QUALQUER
+#FAÇA UM PROGRAMA QUE LEIA UM NUMERO INTEIRO QUALQUER
 #E MOSTRE NA TELA A SUA TABUADA
 
 num = int(input('Escolha um numero: '))
 contador = 1
 print('\t')
 
-print(f'O numero escolhido foi "{num}", a tabuada dele é:')
+print(f'O numero escolhido foi o "{num}", a tabuada dele é:')
 
 while contador <= 10:
     print(f'{num} x {contador} = {num * contador}')
