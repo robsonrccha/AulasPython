542cb5302972af8dafd2431340bbdbdff4b5ba9b
Robson Rocha <robsonrccha@gmail.com>
2025-06-03 16:54:44 -0300

    Aula 07 update

--- Aulas/Aula06.py
+++ Aulas/Aula06.py
@@ -9,7 +9,7 @@ num2 = float(input('Escolha um valor: '))
 #TIPO BOOLEANO
 idade = int(input('Digite sua idade: '))
 maiorIdade = 18
-print(f'Vocé é maior de idade?: {maiorIdade <= idade}\n') #validaçao booleana
+print(f'Vocé é maior de idade?: {maiorIdade <= idade}\n') #VALIDAÇAO BOOLEANA
 
 #TIPO STRING
 msg = 'Olá'
@@ -18,9 +18,9 @@ print('O tipo da variavel "num1" é:' ,type(num1))
 print('O tipo da variavel "num2" é:',type(num2))
 print('O tipo da variavel "msg" é:',type(msg),'\n')
 
-#USO DO "is" PARA STRINGS: alguns exemplos
-print('O valor da variavel "msg" esta todo em letras maiusculas?:' ,msg.isupper()) #verifica se o testo esta todo em maiusculas
-print('O valor da variavel "msg" esta todo em letras minusculas?:' ,msg.islower()) #verifica se esta todo em minuscula
-print('O valor da variavel "msg" é numerico?:' ,msg.isnumeric()) #verifica se é numerico
-print('O valor da variavel "msg" esta em letras(alpha)?:' ,msg.isalpha()) #verifica se esta em letras alfabeticas(se houver espaços ou virgulas será False
-print('O valor da variavel "msg" é alfanumerico?:' ,msg.isalnum(),'\n') #verifica se é alfanumerico
+#USO DO "is" PARA STRINGS: ALGUNS EXEMPLOS
+print('O valor da variavel "msg" esta todo em letras maiusculas?:' ,msg.isupper()) #VERIFICA SE O TESTO ESTA EM MAIUSCULAS
+print('O valor da variavel "msg" esta todo em letras minusculas?:' ,msg.islower()) #VERIFICA SE ESTA EM MINUSCULAS
+print('O valor da variavel "msg" é numerico?:' ,msg.isnumeric()) #VERIFICA SE É NUMERICO
+print('O valor da variavel "msg" esta em letras(alpha)?:' ,msg.isalpha()) #VERIFICA SE ESTA EM LETRAS ALFABETICAS(SE HOUVER ESPAÇOS OU VIRGULAS SERÁ False
+print('O valor da variavel "msg" é alfanumerico?:' ,msg.isalnum(),'\n') #VERIFICA SE É ALFANUMERICO
